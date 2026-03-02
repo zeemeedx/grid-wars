985e06938939fb02124322d0a97198a889418674
zeemeedx <kaua123848@gmail.com>
2026-03-02 19:38:05 -0300

    docker atualizado

---
+++ frontend/main.js
@@ -0,0 +1,10 @@
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+createApp({
+    setup() {
+        const message = ref('Hello Vue!')
+        return {
+            message
+        }
+    }
+}).mount('#app')
